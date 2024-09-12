9b0e9bdc30a8a9029c5e72de8847131fbeaa191e
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-09-12 08:14:37 -0300

    problema do roteamento resolvido

---
+++ src/app/curso/[id]/page.jsx
@@ -0,0 +1,7 @@
+import listaCursos from "@/app/data";
+
+export default function DetalhesCurso({params}){
+  
+  return <p>curso {params.id}</p>;
+
+}
---
+++ src/app/curso/page.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@chakra-ui/react"
+export default function Curso(){
+    return(
+        <Box
+            as="div"
+            minHeight="100%"
+            >
+        </Box>
+    )
+}

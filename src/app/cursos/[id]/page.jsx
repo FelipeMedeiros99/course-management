a0d1eacafdeed671abe3d092a534df21625eb662
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2024-09-15 17:03:03 -0300

    detalhes css

--- src/app/cursos/[id]/page.jsx
+++ src/app/cursos/[id]/page.jsx
@@ -90,8 +90,8 @@ export default function DetalhesCurso({ params, searchParams }) {
           <ModeloBotao 
             onClick={editarCurso} 
             color="white" 
-            bg="#bcbe2a"
-            _hover={{backgroundColor:"#8e9107"}}
+            bg="#fbff29"
+            _hover={{backgroundColor:"#a3a519"}}
             >Editar curso</ModeloBotao>
         </ButtonGroup>
       </Box>

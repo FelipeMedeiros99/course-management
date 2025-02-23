10afc0b8dff5e52611eafd09988ff0c543b4ebc3
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-23 00:37:37 -0300

    refactor: code commited to refactor

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,75 +1,73 @@
-// "use client"
-// // modulos externos
-// import { Box, Spinner } from "@chakra-ui/react"
-// import { useContext, useEffect, useState } from "react";
-// import axios from "axios"
+"use client"
+// modulos externos
+import { Box, Spinner } from "@chakra-ui/react"
+import { useContext, useEffect, useState } from "react";
 
 
-// // modulos internos
+// modulos internos
 // import CaixaCurso from "@/app/Components/CaixaCurso"
-// import Contexto from "../Tools/Contexto";
+import Context from "@/context";
+import axiosConfigs from "@/config/axios.config";
 
 
-// export default function Main() {
-//   // hooks
-//   const [carregandoCursos, setCarregandoCursos] = useState(false)
-//   const { setNavegacaoAtiva, listaCursos, setListaCursos} = useContext(Contexto);
+export default function Main() {
+  // hooks
+  const [loadingCourses, setLoadingCourses] = useState(false)
+  // const { setNavegacaoAtiva, listaCursos, setListaCursos} = useContext(Context);
   
-//   // vars
-//   const linkServer = process.env.NEXT_PUBLIC_LINK_SERVER
+  // effects
+  // useEffect(() => {
+  //   setNavegacaoAtiva(true)
+  // }, [])
 
-//   // effects
-//   useEffect(() => {
-//     setNavegacaoAtiva(true)
-//   }, [])
-
-//   useEffect(() => {
-//     async function carregarCursos() {
-//       setCarregandoCursos(true)
-//       const resposta = await buscarCursos();
-//       setListaCursos(resposta)
-//       setCarregandoCursos(false)
-//     }
-//     carregarCursos()
-//   }, [])
+  useEffect(() => {
+    async function carregarCursos() {
+      setLoadingCourses(true)
+      const resposta = await buscarCursos();
+      setListaCursos(resposta)
+      setLoadingCourses(false)
+    }
+    carregarCursos()
+  }, [])
 
 
-//   // functions
-//   async function buscarCursos() {
-//     try {
-//       const promessa = await axios.get(`${linkServer}/cursos`);
-//       return promessa.data;
-//     } catch (error) {
-//       console.log("erro ao buscar curso: ", error)
-//       return []
-//     }
-//   }
+  // functions
+  async function buscarCursos() {
+    try {
+      const promise = await axiosConfigs.getCourses();
+       console.log(promise.data);
+    } catch (error) {
+      console.log("erro ao buscar curso: ", error)
+      return []
+    }
+  }
 
-//   return (
-//     <Box as="main">
-//       {carregandoCursos &&
-//         <Box
-//           display="flex"
-//           alignItems="center"
-//           justifyContent="center"
-//           height="40vh"
-//         >
+  return (
+    <Box as="main">
+      {/* {loadingCourses &&
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          height="40vh"
+        >
 
-//           <Spinner
-//             width={"100px"}
-//             height={"100px"}
-//             color="#fe7502" />
-//         </Box>
-//       }
-//       <Box
-//         as="div" display="flex" flexWrap="wrap" width="100%"
-//         mx="auto" justifyContent="center" marginTop="40px"
-//         marginBottom="40px"
-//       >
-//         {listaCursos.map((dados, index) => (
-//           <CaixaCurso props={dados} key={index} />
-//         ))}
-//       </Box>
-//     </Box>
-//   );
-// }
+          <Spinner
+            width={"100px"}
+            height={"100px"}
+            color="#fe7502" />
+        </Box>
+      } */}
+
+      {/* <Box
+        as="div" display="flex" flexWrap="wrap" width="100%"
+        mx="auto" justifyContent="center" marginTop="40px"
+        marginBottom="40px"
+      > */}
+        {/* {listaCursos.map((dados, index) => (
+          <CaixaCurso props={dados} key={index} />
+        ))} */}
+      {/* </Box> */}
+    </Box>
+  );
+}

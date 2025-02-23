a03aa79e61628817ec9cbebd2a5bccf22027f662
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-23 00:53:46 -0300

    cleanup: context removed

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,7 +10,6 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 
 import AlertMessage, { AlertMessageInterface } from "@/components/AlertMessage";
-import Context from "@/context";
 import axiosConfigs from "@/config/axios.config";
 
 interface Inputs {
@@ -22,19 +21,17 @@ export default function SignIn() {
   const { register, handleSubmit, formState: { errors } } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = data => validUserDataLogin(data)
 
-  const [carregando, setCarregando] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   const [alertVisibility, setAlertVisibility] = useState(false)
   const [alertMessageParams, setAlertMessageParams] = useState<AlertMessageInterface>({ message: "", status: "neutral" })
-  const { setIsNavigationActive } = useContext(Context)
   const router = useRouter();
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\.[a-zA-Z0-9]+)?$/i
 
   const validUserDataLogin = async (inputs: Inputs) => {
-    setCarregando(true)
+    setIsLoading(true)
     const { email, password } = inputs;
 
     if (email.length >= 3) {
-
       try {
         const response = await axiosConfigs.signIn(inputs)
         localStorage.setItem("userToken", response?.data)
@@ -60,21 +57,14 @@ export default function SignIn() {
               setAlertMessageParams({message: "Erro desconhecido, contate o desenvolvedor", status: "error"})
             }
         }
-
-        // console.log("deu erro: ", e.response || e.request || e)
       } finally {
         setTimeout(()=>setAlertVisibility(false), 4000)
-        setCarregando(false)
+        setIsLoading(false)
       }
 
     }
-    setCarregando(false)
-
+    setIsLoading(false)
   };
-
-  useEffect(() => {
-    setIsNavigationActive(false)
-  }, [setIsNavigationActive])
 
   return (
     <Box
@@ -98,6 +88,7 @@ export default function SignIn() {
         <VStack >
           <Field label="Email" invalid={!!errors.email} errorText={errors?.email?.message}>
             <Input
+              disabled={isLoading}
               {...register("email", {
                 required: { value: true, message: "Obrigatório informar o email" },
                 maxLength: { value: 100, message: "O email deve possuir, no máximo, 100 caracteres." },
@@ -108,6 +99,7 @@ export default function SignIn() {
 
           <Field label="Senha" invalid={!!errors?.password} errorText={errors?.password?.message}>
             <PasswordInput
+              disabled={isLoading}
               {...register("password", {
                 minLength: { value: 6, message: "A senha precisa possuir no mínimo 6 caracteres" },
                 maxLength: { value: 100, message: "A senha não deve ter mais de 100 caracteres" },
@@ -121,11 +113,11 @@ export default function SignIn() {
             _hover={{ backgroundColor: "#124877" }}
             color="white"
             type="submit"
-            disabled={carregando}
+            disabled={isLoading}
             w="100%"
             marginTop="1rem"
           >
-            {!carregando ?
+            {!isLoading ?
               "Confirmar" :
               <Spinner />
             }
@@ -136,17 +128,6 @@ export default function SignIn() {
             <Link href={"/sign-up"}>Não possui cadastro? Cadastre-se!</Link>
           </Box>
         </VStack>
-        {carregando &&
-          <Text
-            wordBreak="break-word"
-            fontSize="sm"
-            color="#525252"
-            maxW="230px"
-            textAlign="center"
-          >
-            Por favor, aguarde! Às vezes o servidor pode apresentar lentidão
-          </Text>
-        }
       </Box>
     </Box>
   );

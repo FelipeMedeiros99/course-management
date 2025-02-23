66f5f47d52d94b0b6f22971729def0c8abad5216
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-23 01:26:25 -0300

    feat: motion.div added internally

--- src/components/AlertMessage/index.tsx
+++ src/components/AlertMessage/index.tsx
@@ -1,24 +1,30 @@
 import { Alert, Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 
-export interface AlertMessageInterface{
+export interface AlertMessageInterface {
   status: "error" | "info" | "warning" | "success" | "neutral" | undefined;
-  message: string
+  message: string;
+  visibility: boolean;
 }
 
-export default function AlertMessage({status, message, ...props}: AlertMessageInterface){
-  return(
-
-
-    <Box {...props}>
-      <Alert.Root status={status} variant="solid">
-        <Alert.Indicator/>
-        <Alert.Content>
-        <Alert.Title fontWeight="700">
-          Atenção!
-        </Alert.Title>
-          <Alert.Description>{message}</Alert.Description>
-        </Alert.Content>
-      </Alert.Root>
-    </Box>
+export default function AlertMessage({ status, message, visibility, ...props }: AlertMessageInterface) {
+  return (
+    <motion.div 
+      initial={{ y: "-25rem", opacity: 0 }}
+      animate={{ y: visibility ? "0rem" : "-25rem", opacity: visibility ? 1 : 0 }}
+      transition={{ duration: 1.5 }}
+    >
+      <Box {...props}>
+        <Alert.Root status={status} variant="solid">
+          <Alert.Indicator />
+          <Alert.Content>
+            <Alert.Title fontWeight="700">
+              Atenção!
+            </Alert.Title>
+            <Alert.Description>{message}</Alert.Description>
+          </Alert.Content>
+        </Alert.Root>
+      </Box>
+    </motion.div>
   )
 }

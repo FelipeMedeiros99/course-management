52519d12b59b7715e50b94feba064b450875a718
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-28 20:32:37 -0300

    style: loading spinner added at courses/id

---
+++ src/app/Tools/index.tsx
@@ -0,0 +1,1 @@
+export const moneyFormat = (value: number) => String(value.toFixed(2)).replace(".", ",")
--- src/app/courses/[id]/page.tsx
+++ src/app/courses/[id]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Box, Text, Button, ButtonGroup, Image, HStack, VStack } from "@chakra-ui/react";
+import { Box, Text, Button, ButtonGroup, Image, HStack, VStack, Spinner } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { InvalidTokenError, jwtDecode } from "jwt-decode"
@@ -12,6 +12,7 @@ import { AxiosError, AxiosResponse } from "axios";
 import { CourseInterface } from "../page";
 import axiosConfigs from "@/config/axios.config";
 import AlertMessage, { AlertMessageInterface } from "@/components/AlertMessage";
+import { moneyFormat } from "@/app/Tools";
 // import adicionarAoCarrinho from "@/app/Tools/adicionarAoCarrinho";
 
 export interface UserData {
@@ -25,48 +26,33 @@ export interface UserData {
 export default function DetalhesCurso({ params }: { params: { id: string } }) {
 
   const router = useRouter()
-
-  // const pathName = usePathname()
-  // const nome = decodeURIComponent(searchParams.nome || '')
-  // const preco = decodeURIComponent(searchParams.preco || '')
-  // const cargaHoraria = decodeURIComponent(searchParams.cargaHoraria || '')
-  // const precoComDesconto = decodeURIComponent(searchParams.precoComDesconto || '')
-  // const conteudo = decodeURIComponent(searchParams.conteudo || '')
-  // const imagem = decodeURIComponent(searchParams.imagem || '')
-  // const id = decodeURIComponent(params.id || '')
-
   const [courseData, setCourseData] = useState<CourseInterface>({ content: "", descountedPrice: 0, id: NaN, name: "", price: NaN, url: "", workload: 0 });
   const [alertVisibility, setAlertVisibility] = useState(false)
   const [alertMessageParams, setAlertMessageParams] = useState<Omit<AlertMessageInterface, "visibility">>({ message: "", status: "neutral" });
-
-
-  const moneyFormat = (value: number) => {
-    return String(value.toFixed(2)).replace(".", ",")
-  }
 
   const addAtCart = async () => {
     try {
       const token = localStorage.getItem("userToken")
       const userData: UserData | null = jwtDecode(token!);
       const response = await axiosConfigs.addCourseAtCart(userData?.id!, courseData.id)
-      console.log(response)
-      if(response.status===202){
+      if (response.status === 202) {
         setAlertVisibility(true)
-        setAlertMessageParams({message: "Curso adicionado ao carrinho", status: "success"})
+        setAlertMessageParams({ message: "Curso adicionado ao carrinho", status: "success" })
       };
     } catch (error: InvalidTokenError | AxiosError | any) {
       setAlertVisibility(true)
-      if(error?.status === 409){
-        return setAlertMessageParams({message: "Este curso já está no seu carrinho", status: "success"})
-      }else if(error instanceof InvalidTokenError){
-        setAlertMessageParams({message: "O token expirou, faça login novamente", status: "error"})
-      }else{
+      if (error?.status === 409) {
+        return setAlertMessageParams({ message: "Este curso já está no seu carrinho", status: "success" })
+      } else if (error instanceof InvalidTokenError) {
+        setAlertMessageParams({ message: "O token expirou, faça login novamente", status: "error" })
+      } else {
+        setAlertMessageParams({ message: "Um erro inesperado aconteceu, tente novamente mais tarde", status: "error" })
         console.log(error)
       }
       router.push("/sign-in")
-      
-    }finally{
-      setTimeout(()=>{
+
+    } finally {
+      setTimeout(() => {
         setAlertVisibility(false)
       }, 4000)
     }
@@ -81,7 +67,6 @@ export default function DetalhesCurso({ params }: { params: { id: string } }) {
       } catch (error: AxiosError | any) {
         setAlertVisibility(true)
         const messageError = error?.response?.data?.message || "";
-
         switch (messageError) {
           case "Invalid token format":
           case "Expired token":
@@ -112,75 +97,80 @@ export default function DetalhesCurso({ params }: { params: { id: string } }) {
       width="100%"
       justifyContent="center"
     >
-      <HStack
-        justifyContent="center"
-        padding="1rem"
-        flexDirection={{ base: "column", md: "row" }}
-        borderRadius="1rem"
-        boxShadow="0 0rem 0.2rem #0000007a"
-      >
-        <AlertMessage message={alertMessageParams.message} status={alertMessageParams.status} visibility={alertVisibility} />
-        <Box
-          as="div"
-          display="flex"
-          alignItems="center"
+      <AlertMessage message={alertMessageParams.message} status={alertMessageParams.status} visibility={alertVisibility} />
+      {courseData.content ?
+        <HStack
           justifyContent="center"
-          width={{ base: "100%", md: "50%" }}
-          maxW="400px"
-          height="auto"
-        >
-          <Image
-            src={courseData.url}
-            alt={courseData.name}
-            width={{ base: "auto", md: "100%" }}
-            height="auto"
-            objectFit="cover"
-            borderRadius="md"
-            boxShadow="md"
-          />
-        </Box>
-        <VStack
-          width={{ base: "100%", md: "50%" }}
-          alignItems={{ base: "center", md: "flex-start" }}
           padding="1rem"
+          flexDirection={{ base: "column", md: "row" }}
+          borderRadius="1rem"
+          boxShadow="0 0rem 0.2rem #0000007a"
         >
-          <Text fontSize="2xl" fontWeight="bold">{courseData.name}</Text>
-          <Text fontSize="md" color="gray.600">{courseData.content}</Text>
-          <Text fontSize="lg" display="flex" height="30px" alignItems="center">
-            <FiClock style={{ marginRight: '8px' }} size="15px" /> Carga Horária: {courseData.workload}
-          </Text>
-          <Text fontSize="lg">10x de <Text as="span" fontWeight="700">R${moneyFormat(courseData.price / 10)}</Text></Text>
-          <Text fontSize="lg" fontWeight="bold" color="green.500">ou R${moneyFormat(courseData.descountedPrice)} à vista</Text>
-          <ButtonGroup
-            width="100%"
+          <Box
+            as="div"
             display="flex"
-            flexDirection={{ base: "column" }}
             alignItems="center"
             justifyContent="center"
+            width={{ base: "100%", md: "50%" }}
+            maxW="400px"
+            height="auto"
           >
-            <Button
+            <Image
+              src={courseData.url}
+              alt={courseData.name}
+              width={{ base: "auto", md: "100%" }}
+              height="auto"
+              objectFit="cover"
+              borderRadius="md"
+              boxShadow="md"
+            />
+          </Box>
+          <VStack
+            width={{ base: "100%", md: "50%" }}
+            alignItems={{ base: "center", md: "flex-start" }}
+            padding="1rem"
+          >
+            <Text fontSize="2xl" fontWeight="bold">{courseData.name}</Text>
+            <Text fontSize="md" color="gray.600">{courseData.content}</Text>
+            <Text fontSize="lg" display="flex" height="30px" alignItems="center">
+              <FiClock style={{ marginRight: '8px' }} size="15px" /> Carga Horária: {courseData.workload}
+            </Text>
+            <Text fontSize="lg">10x de <Text as="span" fontWeight="700">R${moneyFormat(courseData.price / 10)}</Text></Text>
+            <Text fontSize="lg" fontWeight="bold" color="green.500">ou R${moneyFormat(courseData.descountedPrice)} à vista</Text>
+            <ButtonGroup
               width="100%"
-              bg="#206eb3"
-              color="white"
-              onClick={addAtCart}
-              _hover={{ backgroundColor: "#0e3e68" }}
+              display="flex"
+              flexDirection={{ base: "column" }}
+              alignItems="center"
+              justifyContent="center"
             >
-              <FiShoppingCart />
-              Adicionar ao carrinho
-            </Button>
-            <Button
-              width="100%"
-              // onClick={editarCurso}
-              color="white"
-              bg="#e5521e"
-              _hover={{ backgroundColor: "#a73d16" }}
+              <Button
+                width="100%"
+                bg="#206eb3"
+                color="white"
+                onClick={addAtCart}
+                _hover={{ backgroundColor: "#0e3e68" }}
               >
-              <FiEdit />
-              Editar curso
-            </Button>
-          </ButtonGroup>
-        </VStack>
-      </HStack>
+                <FiShoppingCart />
+                Adicionar ao carrinho
+              </Button>
+              <Button
+                width="100%"
+                // onClick={editarCurso}
+                color="white"
+                bg="#e5521e"
+                _hover={{ backgroundColor: "#a73d16" }}
+              >
+                <FiEdit />
+                Editar curso
+              </Button>
+            </ButtonGroup>
+          </VStack>
+        </HStack> :
+        <Spinner width={"7rem"} height={"7rem"} color="#fe7502" />
+      }
+
+
     </VStack>
 
   );

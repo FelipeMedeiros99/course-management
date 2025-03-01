45c768a2630620a03d033de18461dea35846c39c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-02-28 22:04:45 -0300

    feat: renamed functions

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
+import { Box, Heading, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { FiShoppingCart } from "react-icons/fi";
 import { jwtDecode } from "jwt-decode";
 
-import CaixaProdutoCarrinho from "@/components/CaixaProdutoCarrinho";
+import CaixaProdutoCarrinho from "@/components/ProductCartBox";
+import axiosConfigs from "@/config/axios.config";
 
 export interface UserDataInterface {
   id: number,
@@ -35,99 +36,93 @@ export default function CarrinhoUsuario() {
   const [coursesData, setCoursesData] = useState<CourseDataInterface[]>([]);
   const [userData, setUserData] = useState<UserDataInterface>({ name: "", email: "", id: NaN });
 
+  console.log(coursesData)
+  console.log(userData)
+
   useEffect(() => {
-    try{
+    try {
       const token = localStorage.getItem("userToken")
       if (token !== null) {
         const userData = jwtDecode(token) as UserDataInterface
-        console.log(userData)
         setUserData(userData)
       }
-    }catch(e){
+    } catch (e) {
       console.log(e)
     }
   }, []);
 
-  // useEffect(() => {
-  //   if (idUsuario) {
-  //     const buscarProdutos = async () => {
-  //       try {
-  //         const link = `${process.env.NEXT_PUBLIC_LINK_SERVER}/carrinho/${idUsuario}`;
-  //         const resposta = await axios.get(link);
-  //         setCoursesData(resposta.data);
-  //       } catch (e) {
-  //         console.log("Erro ao buscar produtos: ", e);
-  //       } finally {
-  //         setIsLoadingCourses(false);
-  //       }
-  //     };
-
-  //     buscarProdutos();
-  //   }
-  // }, [idUsuario]);
+  useEffect(() => {
+    (async () => {
+      if (userData.id) {
+        try {
+          const response = await axiosConfigs.getUserCart(userData.id);
+          setCoursesData(response.data);
+        } catch (e) {
+          console.log("Erro ao buscar produtos: ", e);
+        } finally {
+          setIsLoadingCourses(false);
+        }
+      };
+    })()
+  }, [userData]);
 
   return (
-    <></>
-    // <Box
-    //   maxW="800px"
-    //   mx="auto"
-    //   p="6"
-    //   boxShadow="lg"
-    //   borderRadius="md"
-    //   bg="gray.50"
-    //   mt="6"
-    // >
-    //   <Heading
-    //     as="h2"
-    //     size="lg"
-    //     mb="4"
-    //     textAlign="center"
-    //     color="#fe7502"
-    //     display="flex"
-    //     alignItems="center"
-    //     justifyContent="center">
-    //     Carrinho
-    //     <FiShoppingCart style={{ marginLeft: "8px" }} />
-    //   </Heading>
+    <HStack w="100%" minH="calc(100% - 12rem)" padding="2rem">
 
-    //   <Text marginBottom="3px">Olá, {userData}!</Text>
-    //   {/* Olá, {userData}! */}
-    //   <Box>
-    //     {isLoadingCourses ? (
-    //       <Box
-    //         display="flex"
-    //         alignItems="center"
-    //         justifyContent="center"
-    //         height="40vh"
-    //       >
-    //         <Spinner
-    //           width={"100px"}
-    //           height={"100px"}
-    //           color="#fe7502"
-    //         />
-    //       </Box>
-    //     ) : (
-    //       coursesData.length > 0 ? (
-    //         coursesData.map(({ codigo_carrinho: idDoCarrinho, url_foto, nome, preco, carga_horaria: cargaHoraria, preco_com_desconto: precoComDesconto, conteudo, id, comprado }, index) => (
-    //           <CaixaProdutoCarrinho
-    //             comprado={comprado}
-    //             idDoCarrinho={idDoCarrinho}
-    //             key={id}
-    //             imagem={url_foto}
-    //             nome={nome}
-    //             preco={preco}
-    //             cargaHoraria={cargaHoraria}
-    //             precoComDesconto={precoComDesconto}
-    //             conteudo={conteudo}
-    //             id={id}
-    //             setCoursesData={setCoursesData}
-    //           />
-    //         ))
-    //       ) : (
-    //         <Text color="#595959">Você ainda não adicionou nada ao seu carrinho</Text>
-    //       )
-    //     )}
-    //   </Box>
-    // </Box>
+      <VStack
+        maxW="50rem"
+        w="100%"
+        minH="100%"
+        mx="auto"
+        p="6"
+        boxShadow="lg"
+        borderRadius="md"
+        bg="gray.50"
+        mt="6"
+      >
+        <Heading
+        as="h2"
+        size="lg"
+        mb="4"
+        textAlign="center"
+        color="#fe7502"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        Carrinho
+        <FiShoppingCart style={{ marginLeft: "0.5rem" }} />
+      </Heading>
+
+      <Box>
+         {isLoadingCourses ? (
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            height="40vh"
+          >
+            <Spinner
+              width={"100px"}
+              height={"100px"}
+              color="#fe7502"
+            />
+          </Box>
+        ) : (
+          coursesData.length > 0 ? (
+            coursesData.map((courseData, index) => (
+              <CaixaProdutoCarrinho
+                courseData={courseData}
+                userId={userData.id}
+                setCoursesData={setCoursesData}
+              />
+            ))
+          ) : (
+            <Text color="#595959">Você ainda não adicionou nada ao seu carrinho</Text>
+          )
+        )}
+      </Box>
+      </VStack>
+    </HStack>
   );
 }
--- src/components/ProductCartBox/index.tsx
+++ src/components/ProductCartBox/index.tsx
@@ -1,124 +1,129 @@
 "use client"
 import { useState } from "react";
-import { Box, Text, Image, ButtonGroup, Spinner} from "@chakra-ui/react";
-import ModeloBotao from "../ModeloBotao";
+import { Box, Text, Image, ButtonGroup, Spinner, Button } from "@chakra-ui/react";
 import axios from "axios";
 import { moneyFormat } from "@/app/Tools";
+import { CourseDataInterface } from "@/app/cart/page";
+
+
+interface ProductCartBoxInterface {
+  courseData: CourseDataInterface;
+  userId: number;
+  setCoursesData: (newData: CourseDataInterface[]) => void;
+}
+
+export default function ProductCartBox({ courseData, userId, setCoursesData }: ProductCartBoxInterface) {
+
+  const [carregando, setCarregando] = useState(false);
+  const { course } = courseData;
+
+  // async function removerDoCarrinho() {
+  //     setCarregando(true)
+  //     try {
+  //         const promisse = await axios.delete(link)
+  //         //atualizando carrinho
+  //         setCursosNoCarrinho(promisse.data)
+  //     } catch (e) {
+  //         console.log("erro ao tentar remover do carrinho", e)
+  //     }
+  //     setCarregando(false)
+  //     // setProdutoSelecionado()
+  // }
 
 
 
-export default function ProductCartBox({ courseData, userId, setCoursesData}) {
-    const link = `${process.env.NEXT_PUBLIC_LINK_SERVER}/carrinho?id=${idDoCarrinho}&usuario_id=${JSON.parse(localStorage.getItem("usuario")).id}`
-    const [carregando, setCarregando] = useState(false)
-
-    async function removerDoCarrinho() {
-        setCarregando(true)
-        try {
-            const promisse = await axios.delete(link)
-            //atualizando carrinho
-            setCursosNoCarrinho(promisse.data)
-        } catch (e) {
-            console.log("erro ao tentar remover do carrinho", e)
-        }
-        setCarregando(false)
-        // setProdutoSelecionado()
-    }
+  // async function fecharPedido() {
+  //     setCarregando(true)
+  //     try {
+  //         const promisse = await axios.put(link)
+  //         setCursosNoCarrinho(promisse.data)
+  //     } catch (e) {
+  //         console.log("Erro ao fechar pedido: ", e)
+  //     }
+  //     setCarregando(false)
+  // }
 
 
+  return (
+    <Box
+      display="flex"
+      flexDir={{ md: "row", base: "column", }}
+      alignItems="center"
+      justifyContent={{ sm: "center", md: "left" }}
+      as="div"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="md"
+      p="4"
+      mb="6"
+      bg="white"
+      transition="transform 0.2s"
+      _hover={{ transform: "scale(1.03)" }}
 
-    async function fecharPedido() {
-        setCarregando(true)
-        try {
-            const promisse = await axios.put(link)
-            setCursosNoCarrinho(promisse.data)
-        } catch (e) {
-            console.log("Erro ao fechar pedido: ", e)
-        }
-        setCarregando(false)
-    }
+    >
+      <Image src={course.url} alt={course.name} borderRadius="md" mb="4" mr="8" maxW="300px" />
 
+      <Box width="100%">
+        <Text fontSize="xl" fontWeight="bold" mb="2">
+          {course.name}
+        </Text>
 
-    return (
-        <Box
-            display="flex"
-            flexDir={{ md: "row", base: "column", }}
-            alignItems="center"
-            justifyContent={{ sm: "center", md: "left" }}
-            as="div"
-            borderWidth="1px"
-            borderRadius="lg"
-            overflow="hidden"
-            boxShadow="md"
-            p="4"
-            mb="6"
-            bg="white"
-            transition="transform 0.2s"
-            _hover={{ transform: "scale(1.03)" }}
-
-        >
-            <Image src={imagem} alt={nome} borderRadius="md" mb="4" mr="8" maxW="300px" />
-
-            <Box width="100%">
-                <Text fontSize="xl" fontWeight="bold" mb="2">
-                    {nome}
-                </Text>
-
-                <Box as="p" fontSize="lg" color="gray.600">
-                    10x de <Text as="span" fontSize="xl" fontWeight="bold"> R$ {moneyFormat(Number(preco) / 10)}</Text>
-                </Box>
-                <Box as="p" color="green.600" fontSize="lg" fontWeight="bold">
-                    ou R$ {moneyFormat(precoComDesconto)} à vista
-                </Box>
-
-                
-
-                <Text fontSize="sm" color="gray.600" mb="2">
-                    Carga Horária: {cargaHoraria}
-                </Text>
-
-                <Text fontSize="xs" color="gray.400">
-                    ID do Produto: {id}
-                </Text>
-
-
-                {!comprado ?
-                    <ButtonGroup
-                        isDisabled={carregando}
-                        display="flex"
-                        flexDir={{ base: "column", sm: "row"}}
-                        alignItems="center"
-                        justifyContent="center"
-                        >
-                        <ModeloBotao
-                            onClick={fecharPedido}
-                            color="white"
-                            minW="150px"
-                            backgroundColor="#206eb3"
-                            _hover={{ backgroundColor: "#175388" }}
-                            
-                        >
-                            {carregando ?
-                                <Spinner /> :
-                                "Fechar pedido"
-                            }
-                        </ModeloBotao>
-                        <ModeloBotao
-                            onClick={removerDoCarrinho}
-                            color="white"
-                            minW="150px"
-                            backgroundColor="#ff4500"
-                            _hover={{ backgroundColor: "#ad2f02" }}
-                        >
-                            {carregando ?
-                                <Spinner /> :
-                                "Remover do carrinho"}
-                        </ModeloBotao>
-                    </ButtonGroup> :
-
-                    <Text color={"#28a745"}>Você já possui este curso!</Text>
-                }
-            </Box>
-
+        <Box as="p" fontSize="lg" color="gray.600">
+          10x de <Text as="span" fontSize="xl" fontWeight="bold"> R$ {moneyFormat(Number(course.price) / 10)}</Text>
         </Box>
-    );
+        <Box as="p" color="green.600" fontSize="lg" fontWeight="bold">
+          ou R$ {moneyFormat(course.descountedPrice)} à vista
+        </Box>
+
+
+
+        <Text fontSize="sm" color="gray.600" mb="2">
+          Carga Horária: {course.workload}
+        </Text>
+
+        <Text fontSize="xs" color="gray.400">
+          ID do Produto: {course.id}
+        </Text>
+
+
+        {!courseData.isOrderCompleted ?
+          <ButtonGroup
+            display="flex"
+            flexDir={{ base: "column", sm: "row" }}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Button
+              // onClick={fecharPedido}
+              color="white"
+              minW="150px"
+              backgroundColor="#206eb3"
+              _hover={{ backgroundColor: "#175388" }}
+
+            >
+              {carregando ?
+                <Spinner /> :
+                "Fechar pedido"
+              }
+            </Button>
+            <Button
+              // onClick={removerDoCarrinho}
+              color="white"
+              minW="150px"
+              backgroundColor="#ff4500"
+              _hover={{ backgroundColor: "#ad2f02" }}
+            >
+              {carregando ?
+                <Spinner /> :
+                "Remover do carrinho"}
+            </Button>
+          </ButtonGroup> :
+
+          <Text color={"#28a745"}>Você já possui este curso!</Text>
+        }
+      </Box>
+
+    </Box>
+  );
 }

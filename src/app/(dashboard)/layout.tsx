b02097a3a9ee1f9a65943941cce43de36c38eb46
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-05 00:09:27 -0300

    cleanup: unused imports removed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,6 @@
 "use client"
-import { useState } from 'react';
-import { Provider } from '@/components/ui/provider';
-
-import { ColorModeProvider } from '@/components/ui/color-mode';
-import { Box, Theme } from '@chakra-ui/react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { relative } from 'path';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
